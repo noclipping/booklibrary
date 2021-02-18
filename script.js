4e215787d5572feebd4a01343dfa5b9511f18653
aresus <aresous@gmail.com>
2021-02-18 04:13:26 -0500

    adding button functionality

--- script.js
+++ script.js
@@ -15,10 +15,25 @@ Book.prototype.tap = function(){
 	return {title:this.title,author:this.author,pages:this.pages,read:this.read};
 }
 
+function addBookToLibrary(title,author,pages,read){
+	let newBook= new Book(title, author, pages, read)
+	library.push(newBook.tap());
+}
+
+
 let theTable = document.querySelector("table");
+
 
 let newbook = new Book("Call of C", "HP love", 234, false);
 
+let submitButton = document.getElementById("button")
+
+submitButton.addEventListener("click",e=>{
+	let title = document.getElementById("title");
+  let author = document.getElementById("author");
+  console.log(author.value);
+  console.log(title.value);
+})
 
 
 
